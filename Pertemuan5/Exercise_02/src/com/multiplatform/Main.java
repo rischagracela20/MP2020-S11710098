e37e00a95f6098c1494206b1945d0477c7ece0ee
rischagracela20 <gracelarischa20@gmail.com>
2020-06-10 16:00:04 +0800

    Exercise 2 Pertemuan 5

---
+++ Pertemuan5/Exercise_02/src/com/multiplatform/Main.java
@@ -0,0 +1,10 @@
+package com.multiplatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double myDoubleNumber = 150d;
+	    double convertedToKilograms = myDoubleNumber * 0.45359237;
+        System.out.println("Result = " + convertedToKilograms);
+    }
+}
