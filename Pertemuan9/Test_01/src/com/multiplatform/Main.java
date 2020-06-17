5dacdff5aa6eb13e29d9de1041812ad8afa7778b
rischagracela20 <gracelarischa20@gmail.com>
2020-06-17 16:26:00 +0800

    Test_01 Number 2

--- Pertemuan9/Test_01/src/com/multiplatform/Main.java
+++ Pertemuan9/Test_01/src/com/multiplatform/Main.java
@@ -3,25 +3,50 @@ package com.multiplatform;
 public class Main {
 
     public static void main(String[] args) {
+        //
+        System.out.println("Barking_Dog :");
         System.out.println(shouldWakeUp(true, 1));
         System.out.println(shouldWakeUp(false,2));
         System.out.println(shouldWakeUp(true, 8));
         System.out.println(shouldWakeUp(true, -1));
+        //
+        System.out.println("Playing_Cat :");
+        System.out.println(isCatPlaying(true,10));
+        System.out.println(isCatPlaying(false,36));
+        System.out.println(isCatPlaying(false,35));
 
     }
     //1.Barking Dog
-    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
-        if(barking){
-            if ((hourOfDay<8&&hourOfDay>=0) || (hourOfDay>22&&hourOfDay<24)){
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (barking) {
+            if ((hourOfDay < 8 && hourOfDay >= 0) || (hourOfDay > 22 && hourOfDay < 24)) {
                 System.out.println("Time to Wake up, dog is barking");
-         return true; }
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    //2.Playing cat
+        public static boolean isCatPlaying(boolean summer, int temperature){
+        if (summer){
+            if (temperature >= 25 && temperature <= 45){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else
+        if(temperature >= 25 && temperature <= 35){
+            return true;
+        }
         else {
             return false;
         }
     }
-    else{
-        return  false;
-        }
 
-    }
 }
+
